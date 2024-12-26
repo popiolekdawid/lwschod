a7e5ee423e8e71ed8bf675baab9cb1c041169560
Dawid Popiołek <dawidpopiolek@protonmail.com>
2024-12-26 20:26:32 +0100

    setup gh actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-// https://astro.build/config
 export default defineConfig({
-    site: 'http://lwschod.pl',
+    site: 'https://popiolekdawid.github.io',
+    base: '/lwschod',
 })
